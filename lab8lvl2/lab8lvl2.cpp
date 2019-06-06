21d531971dc0d89b997cb37acb436cda1db06341
kote6725@gmail.com <kote6725@gmail.com>
2019-06-07 00:57:19 +0300

    Добавьте файлы проекта.

---
+++ lab8lvl2/lab8lvl2.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <list>
+
+int main()
+{
+	std::list<int> flist{ 2,3,-1,3,-4,4,-4,-2,5,5,1 };
+	std::list<int> reslist;
+	bool flag = false;
+	for (auto& iter : flist)
+	{
+		if (!flag)
+		{
+			reslist.push_back(iter);
+		}
+		flag = iter == 4;
+	}
+	reslist.reverse();
+	flist = reslist;
+	reslist.clear();
+	flag = false;
+	for (auto& iter : flist)
+	{
+		if (flag)
+		{
+			reslist.push_back(0);
+		}
+		reslist.push_back(iter);
+		flag = iter == 1;
+	}
+	reslist.reverse();
+	for (auto& iter : reslist)
+	{
+		std::cout << iter << '\n';
+	}
+}
